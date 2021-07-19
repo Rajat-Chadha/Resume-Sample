0c502b4e7df062df17414befdd1051d8b43534b6
Rajat-Chadha <rajat.chadha2001@gmail.com>
2021-07-19 10:12:17 +0530

    added smooth scroll

---
+++ script.js
@@ -0,0 +1,95 @@
+function about(){
+  var y=10;
+  var Y=380;
+  var scrollInterval = setInterval(function() {
+    if(y>=Y){
+      clearInterval(scrollInterval);
+      return;
+    }
+    else{
+    y+=10;
+    window.scrollBy(0,10);
+    }
+    }, 0.1);
+  
+  }
+  
+function skills(){
+  var y=10;
+  var Y=830;
+  var scrollInterval = setInterval(function() {
+    if(y>=Y){
+      clearInterval(scrollInterval);
+      return;
+    }
+    else{
+    y+=10;
+    window.scrollBy(0,10);
+    }
+    }, 0.1);
+  
+  }
+
+function experience(){
+  var y=10;
+  var Y=1300;
+  var scrollInterval = setInterval(function() {
+    if(y>=Y){
+      clearInterval(scrollInterval);
+      return;
+    }
+    else{
+    y+=10;
+    window.scrollBy(0,10);
+    }
+    }, 0.1);
+  
+  }
+
+function education(){
+  var y=10;
+  var Y=2190;
+  var scrollInterval = setInterval(function() {
+    if(y>=Y){
+      clearInterval(scrollInterval);
+      return;
+    }
+    else{
+    y+=10;
+    window.scrollBy(0,10);
+    }
+    }, 0.1);
+  
+  }
+
+function portfolio(){
+  var y=10;
+  var Y=3090;
+  var scrollInterval = setInterval(function() {
+    if(y>=Y){
+      clearInterval(scrollInterval);
+      return;
+    }
+    else{
+    y+=10;
+    window.scrollBy(0,10);
+    }
+    }, 0.1);
+  
+  }
+
+function contact(){
+  var y=10;
+  var Y=3890;
+  var scrollInterval = setInterval(function() {
+    if(y>=Y){
+      clearInterval(scrollInterval);
+      return;
+    }
+    else{
+    y+=10;
+    window.scrollBy(0,10);
+    }
+    }, 0.1);
+  
+  }
